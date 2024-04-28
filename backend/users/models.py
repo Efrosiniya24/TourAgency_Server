53800be7c88008e95e5e225b4f40a030f593b041
frosya <efrosiniya24@gmail.com>
2024-04-29 02:02:02 +0300

    AllUserView, SearchUserView

---
+++ backend/users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255, unique=True)
+    phone = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255, unique=False)
+    patronymic = models.CharField(max_length=255, unique=False)
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import SignUp, SignIn, UserView, LogoutView
+from .views import SignUp, SignIn, UserView, LogoutView, AllUsersView, SearchUserView
 
 urlpatterns = [
     path('signIn', SignIn.as_view()),
     path('signUp', SignUp.as_view()),
     path('user', UserView.as_view()),
     path('logout', LogoutView.as_view()),
+    path('allUsers', AllUsersView.as_view()),
+    path('searchUser', SearchUserView.as_view()),
 ]
